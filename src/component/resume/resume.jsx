a31a693e808338e55dba0372cb7f3d550392a864
danarbastian7 <111851825+danarbastian7@users.noreply.github.com>
2024-02-13 17:51:57 +0900

    fourth commit

--- src/component/resume/resume.jsx
+++ src/component/resume/resume.jsx
@@ -1,17 +1,78 @@
 import { Container, Grid, Typography } from "@mui/material";
 import React from "react";
 import "./resume.css";
+import resumeDate from "../../utils/resumeDate";
+import CustomTimeline, {
+  CustomTimelineSeparator,
+} from "../../component/timeline/timeline";
+import WorkIcon from "@mui/icons-material/Work";
+import {
+  TimelineContent,
+  TimelineDot,
+  TimelineItem,
+  TimelineSeparator,
+} from "@mui/lab";
 
 const Resume = () => {
   return (
     <>
-      <Grid container className="">
-        <Grid item className="section_title">
+      <Grid container className="section pb_45">
+        <Grid item className="section_title mb_30">
           <span></span>
-          <Typography variant="h6">About me</Typography>
+          <Typography variant="h6" className="section_title_text">
+            About me
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body2" className="aboutme_text">
+            {resumeDate.about}
+          </Typography>
         </Grid>
       </Grid>
+      {/* education and experience */}
+      <Grid container className="section">
+        <Grid item className="section_title mb_30">
+          <span></span>
+          <Typography variant="h6" className="section_title_text">
+            Resume
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container className="resume_timeline">
+            {/* Experiences */}
+            <Grid item sm={12} md={6}>
+              <CustomTimeline title={"Work History"} icon={<WorkIcon />}>
+                {resumeDate.experiences.map((experience) => (
+                  <TimelineItem>
+                    <CustomTimelineSeparator />
+                    <TimelineContent>
+                      <Typography className="timeline_title">
+                        {experience.title}
+                      </Typography>
+                      <Typography variant="caption" className="timeline_date">
+                        {experience.date}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        className="timeline_description"
+                      >
+                        {experience.description}
+                      </Typography>
+                    </TimelineContent>
+                  </TimelineItem>
+                ))}
+              </CustomTimeline>
+            </Grid>
+            {/* Education */}
+            <Grid item sm={12} md={6}></Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      {/* Services */}
       <Grid container className=""></Grid>
+      {/* Skills */}
+      <Grid container className=""></Grid>
+      {/* Contact */}
       <Grid container className=""></Grid>
     </>
   );
--- src/utils/resumeDate.js
+++ src/utils/resumeDate.js
@@ -33,4 +33,52 @@ export default {
       icon: <GitHubIcon />,
     },
   },
+  about:
+    "Velit voluptate dolor enim nostrud officia et Lorem do id labore esse aliquip aliquip.Aliquip excepteur laboris dolore in laborum sint ipsum. Non magna sunt fugiat magna duis commodo sint. Est cupidatat nulla laborum aliqua. Velit adipisicing sint cupidatat reprehenderit. Nulla qui amet culpa do enim quis minim sit. Eiusmod nostrud nulla non eu sint.Lorem adipisicing anim nulla non Lorem cillum. Sint eiusmod eu eiusmod et ea aliqua incididunt laborum Lorem voluptate officia sint commodo. In consequat sint sit sint ullamco incididunt. Commodo do cupidatat laborum ut et amet dolor. Aute ea id sit aute. Mollit Lorem cillum ad pariatur dolor aute dolor Lorem commodo amet deserunt dolor reprehenderit laborum. Aliquip exercitation sint aliqua anim in nostrud sunt aliqua fugiat dolor.",
+
+  experiences: [
+    {
+      title: "IoT Engineer",
+      office: "Kani Construction",
+      date: "July 2023 - Present",
+      description:
+        "Delivered new ideas for the sustainability especially in construction works.",
+    },
+    {
+      title: "FullStack Developer",
+      office: "Purwadhika Digital Tech.",
+      date: "July 2022 - February 2023",
+      description:
+        "Developed website with end-to-end feature, built database concept as back end engineer and serve the data as front end engineer",
+    },
+    {
+      title: "Engineering",
+      office: "PT Yasa Patria Perkasa",
+      date: "October 2021 - July 2022",
+      description:
+        "Main window person responsible in Biding proposal creation and on-the ground survey.",
+    },
+    {
+      title: "Project Control",
+      office: "PT Jaya Konstruksi",
+      date: "January 2019 - October 2021",
+      description:
+        "Designed the project plan and lead 10 engineers to deliver the project in time with timely PDCA.",
+    },
+  ],
+  education: [
+    {
+      title: "Civil Engineering",
+      office: "Universitas Gadjah Mada",
+      date: "July 2014 - November 2018",
+      description:
+        "Graduated in bachelor from the best university in Indonesia with GPA: 3.38/4.00.",
+    },
+    {
+      title: "Full Stack Developer - Purwadhika Digital Tech.",
+      date: "July 2022 - December 2022",
+      description:
+        "Course graduated as full stack web development with the updated curriculum and qualified mentor.",
+    },
+  ],
 };
